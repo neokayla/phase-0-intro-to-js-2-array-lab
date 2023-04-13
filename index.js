176b358aae7d9e0aa26cf3ee961297a32643ea27
Kayla Lebron <kayla.lebron12@gmail.com>
2023-04-13 16:28:59 -0400

    wassup

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+function destructivelyAppendCat(name) {
+    cats.push(name)
+}
+function destructivelyPrependCat(name) {
+    cats.unshift(name)
+}
+function destructivelyRemoveLastCat() {
+    cats.pop()
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+}
+function appendCat(name) {
+    return appendCat = [...cats, name]
+}
+function prependCat(name) {
+    return prependCat = [name, ...cats]
+}
+function removeLastCat() {
+    return removeLastCat = cats.slice(0,-1)
+}
+function removeFirstCat() {
+    return removeFirstCat = cats.slice(1)
+}
